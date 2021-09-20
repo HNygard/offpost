b2258e9270e671c3737fc2cb581e5360e63b2498
Hallvard Nygard <hallvard.nygard@gmail.com>
2021-09-20 16:51:17 +0200

    Email engine: Thread starter takes more params

--- organizer/src/start-thread.php
+++ organizer/src/start-thread.php
@@ -8,12 +8,12 @@ if (!isset($_POST['entity_id'])) {
     ?>
     <form method="POST">
         <h1>Start email thread</h1>
-        <input type="text" name="title" value=""> - Title<br>
+        <input type="text" name="title" value="<?= htmlescape(isset($_GET['title']) ? $_GET['title'] : '') ?>"> - Title<br>
         <input type="text" name="my_name" value="<?= htmlescape(isset($_GET['my_name']) ? $_GET['my_name'] : '') ?>"> - My name<br>
         <input type="text" name="my_email" value="<?= htmlescape($_GET['my_email']) ?>"> - My email<br>
-        <input type="text" name="labels" value=""> - Labels, space separated<br>
-        <input type="text" name="entity_id" value=""> - Entity id<br>
-        <input type="text" name="entity_title_prefix" value=""> - Entity title prefix (only used if first thread for this entity)<br>
+        <input type="text" name="labels" value="<?= htmlescape(isset($_GET['labels']) ? $_GET['labels'] : '') ?>"> - Labels, space separated<br>
+        <input type="text" name="entity_id" value="<?= htmlescape(isset($_GET['entity_id']) ? $_GET['entity_id'] : '') ?>"> - Entity id<br>
+        <input type="text" name="entity_title_prefix" value="<?= htmlescape(isset($_GET['entity_title_prefix']) ? $_GET['entity_title_prefix'] : '') ?>"> - Entity title prefix (only used if first thread for this entity)<br>
         <input type="submit" value="Create thread">
     </form>
     <?php
