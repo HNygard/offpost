53e082f26443b580c0b03cca4d4e42225fab3663
Hallvard Nygard <hallvard.nygard@gmail.com>
2025-03-14 21:57:48 +0100

    Test PDFs should render

--- organizer/src/e2e-tests/ThreadEmailReceiveIntegrationTest.php
+++ organizer/src/e2e-tests/ThreadEmailReceiveIntegrationTest.php
@@ -182,7 +182,53 @@ class ThreadEmailReceiveIntegrationTest extends TestCase {
         $subject = 'Test Receive Email ' . $uniqueId;
         $plainBody = 'This is a test email for receiving process';
         $attachmentName = 'test.pdf';
-        $attachmentContent = '%PDF-1.4 Test PDF content';
+        $attachmentContent = "%PDF-1.4
+%âãÏÓ
+
+1 0 obj
+  << /Type /Catalog
+     /Pages 2 0 R
+  >>
+endobj
+
+2 0 obj
+  << /Type /Pages
+     /Kids [3 0 R]
+     /Count 1
+  >>
+endobj
+
+3 0 obj
+  << /Type /Page
+     /Parent 2 0 R
+     /MediaBox [0 0 612 792]
+     /Contents 4 0 R
+     /Resources << >>
+  >>
+endobj
+
+4 0 obj
+  << /Length 22 >>
+stream
+(Hello, PDF!) Tj
+endstream
+endobj
+
+xref
+0 5
+0000000000 65535 f 
+0000000010 00000 n 
+0000000060 00000 n 
+0000000110 00000 n 
+0000000160 00000 n 
+
+trailer
+  << /Root 1 0 R
+     /Size 5
+  >>
+startxref
+210
+%%EOF";
         
         // Generate unique message ID and thread index
         $messageId = '<' . uniqid() . '@test.local>';
--- organizer/src/e2e-tests/pages/common/E2ETestSetup.php
+++ organizer/src/e2e-tests/pages/common/E2ETestSetup.php
@@ -98,7 +98,53 @@ class E2ETestSetup {
         
         // Create attachment file
         $attachmentFile = $threadDir . '/' . date('Y-m-d_His', $email_time) . ' - IN - att 1-754dc77d28e62763c4916970d595a10f.pdf';
-        file_put_contents($attachmentFile, '%PDF-1.4 Test PDF content');
+        file_put_contents($attachmentFile, "%PDF-1.4
+%âãÏÓ
+
+1 0 obj
+  << /Type /Catalog
+     /Pages 2 0 R
+  >>
+endobj
+
+2 0 obj
+  << /Type /Pages
+     /Kids [3 0 R]
+     /Count 1
+  >>
+endobj
+
+3 0 obj
+  << /Type /Page
+     /Parent 2 0 R
+     /MediaBox [0 0 612 792]
+     /Contents 4 0 R
+     /Resources << >>
+  >>
+endobj
+
+4 0 obj
+  << /Length 22 >>
+stream
+(Hello, PDF!) Tj
+endstream
+endobj
+
+xref
+0 5
+0000000000 65535 f 
+0000000010 00000 n 
+0000000060 00000 n 
+0000000110 00000 n 
+0000000160 00000 n 
+
+trailer
+  << /Root 1 0 R
+     /Size 5
+  >>
+startxref
+210
+%%EOF");
         
         // Grant access to the user
         $thread->addUser($userId, true);
