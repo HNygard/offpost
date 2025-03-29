22b5bc98c735514fd23fbfb3b3ef58f39fc2e29e
Hallvard Nygard <hallvard.nygard@gmail.com>
2025-03-29 21:54:17 +0100

    Scheduled email receiver: Do 3 folders at a time

--- organizer/src/system-pages/scheduled-email-receiver.php
+++ organizer/src/system-pages/scheduled-email-receiver.php
@@ -13,6 +13,25 @@ $emailReceiver = new ThreadScheduledEmailReceiver();
 // Note: We only process one folder at a time to avoid overloading the system
 $result = $emailReceiver->processNextFolder();
 
+if (!$result['success']) {
+    // Output the result
+    header('Content-Type: application/json');
+    echo json_encode($result, JSON_PRETTY_PRINT);
+    exit;
+}
+
+$result2 = $emailReceiver->processNextFolder();
+
+$results = array(
+    $result,
+    $result2
+);
+
+if ($result2['success']) {
+    $result3 = $emailReceiver->processNextFolder();
+    $results[] = $result3;
+}
+
 // Output the result
 header('Content-Type: application/json');
-echo json_encode($result, JSON_PRETTY_PRINT);
+echo json_encode($results, JSON_PRETTY_PRINT);
