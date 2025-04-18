348c7d50fa2e351e15617fd1616724ad912559fc
Hallvard Nygard <hallvard.nygard@gmail.com>
2025-04-18 11:52:18 +0200

    Bump styles

--- organizer/src/head.php
+++ organizer/src/head.php
@@ -7,4 +7,4 @@ if (!isset($pageTitle)) {
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?= $pageTitle ?></title>
 <link rel="icon" type="image/png" href="/images/offpost-favicon.png">
-<link href="/css/style.css?2" rel="stylesheet">
+<link href="/css/style.css?3" rel="stylesheet">
