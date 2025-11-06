8807748ef5be489cc1984a701b44bb6d9fcbf46d
Hallvard Nygård <168380+HNygard@users.noreply.github.com>
2025-11-06 19:32:34 +0000

    one more

--- organizer/src/tests/ThreadEmailMoverTest.php
+++ organizer/src/tests/ThreadEmailMoverTest.php
@@ -60,7 +60,7 @@ class ThreadEmailMoverTest extends TestCase {
 
         // Verify mapping
         $this->assertArrayHasKey('test1@example.com', $mapping);
-        $this->assertEquals('INBOX.test-entity - Thread 1', $mapping['test1@example.com']);
+        $this->assertEquals('INBOX.000000000-test-entity-development - Thread 1', $mapping['test1@example.com']);
         
         // Archived threads should not be in mapping
         $this->assertArrayNotHasKey('test2@example.com', $mapping);
